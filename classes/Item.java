c9ca7052c4f65007ddfa94d140778914b0c1701a
Batista <140225853+Batissta@users.noreply.github.com>
2023-08-25 12:37:10 -0300

    Create Item.java

---
+++ classes/Item.java
@@ -0,0 +1,39 @@
+package classes;
+
+public class Item {
+	private String nome;
+	private long codigo;
+	private int estoque;
+	
+	public Item(String nome, int cod){
+		this.setNome(nome);
+		this.setCodigo((long)cod);
+		this.setEstoque(0);
+	}
+	
+	protected String getNome() {
+		return nome;
+	}
+	protected void setNome(String nome) {
+		this.nome = nome;
+	}
+	protected long getCodigo() {
+		return codigo;
+	}
+	protected void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	protected int getEstoque() {
+		return estoque;
+	}
+	protected void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+	
+	protected void addEstoque(int n) {
+		this.setEstoque(getEstoque() + n);
+	}
+	protected void venderEstoque(int n) {
+			this.setEstoque(getEstoque() - n);
+	}
+}
