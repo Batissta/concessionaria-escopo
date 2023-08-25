23cccc21e9530bc19830942bdb9e3b5cd40ccc01
Batista <140225853+Batissta@users.noreply.github.com>
2023-08-25 12:39:43 -0300

    Onde o código é executado

---
+++ executaProjeto/App.java
@@ -0,0 +1,12 @@
+package executaProjeto;
+
+import classes.Comercio;
+
+public class App {
+
+	public static void main(String[] args) {
+		Comercio concessionaria = new Comercio();
+		concessionaria.menu();
+	}
+
+}
