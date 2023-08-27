a78a0fff18e8c92312e8db8283f992e99ba25f86
Batista <140225853+Batissta@users.noreply.github.com>
2023-08-27 19:29:30 -0300

    Update Comercio.java

--- classes/Comercio.java
+++ classes/Comercio.java
@@ -1,47 +1,46 @@
 package classes;
 
-import java.util.Scanner;
+import util.Uteis;
 
 public class Comercio {
 	private Itens produtos = new Itens();
-	private Scanner t = new Scanner(System.in);
 	
-	public void mostrarLinha() {
-		System.out.println("------------------------------------------------");
-	}
-	private int leInt(String msg) {
-		System.out.print(msg);
-		int num = Integer.parseInt(t.nextLine());
-		return num;
-	}
 	public void menu() {
-		while (true) {
+		int esc = 6;
+		do {
 			System.out.printf(
 					"%s\n1) Listar carros cadastrados.\n"+ "2) Cadastrar novo carro.\n"+ "3) Adicionar estoque a um carro existente.\n"+ "4) Remover um produto do comércio.\n"
 					+ "5) Vender algum produto existente.\n"+ "6) Sair\n", "MENU - Concessionária FUTURE");
-			int esc = leInt("Sua escolha: ");
-			mostrarLinha();
-			if (esc == 1) {
+			esc = Uteis.leInt("Sua escolha: ");
+			Uteis.mostrarLinha();
+			switch (esc) {
+			case 1:
 				listar();
-				mostrarLinha();
-			}else if (esc == 2) {
+				Uteis.mostrarLinha();
+				break;
+			case 2:
 				cadastrar();
-				mostrarLinha();
-			}else if (esc == 3) {
+				Uteis.mostrarLinha();
+				break;
+			case 3:
 				mudaEstoque("adicionar");
-				mostrarLinha();
-			}else if (esc == 4) {
+				Uteis.mostrarLinha();
+				break;
+			case 4:
 				remover();
-				mostrarLinha();
-			}else if (esc == 5) {
+				Uteis.mostrarLinha();
+				break;
+			case 5:
 				mudaEstoque("vender");
-				mostrarLinha();
-			}else if (esc == 6) {
+				Uteis.mostrarLinha();
+				break;
+			case 6:
 				System.out.println("Saindo do sistema...");
 				break;
-			}else {
-				System.out.println("Opção inválida...");}
-	}
+			default:
+				System.out.println("Opção inválida...");
+			}
+	} while (esc !=6);
 }
 	public void listar() {
 		if (produtos.getProdutos().size() == 0) {
@@ -51,15 +50,14 @@ public class Comercio {
 		}
 	}
 	public void cadastrar() {
-		System.out.print("Digite o nome do carro: ");
-		String nomedoCarro = t.nextLine();
+		String nomedoCarro = Uteis.leString("Digite o nome do carro: ");
 		while (true) {
-			int codigodoCarro = leInt("Digite o código do carro: ");
+			int codigodoCarro = Uteis.leInt("Digite o código do carro: ");
 			if (produtos.getProdutos().size()>0) {
-				if (produtos.codigoExiste(codigodoCarro)) {
+				if (produtos.findCod(codigodoCarro) != null) {
 					System.out.printf("Já temos o carro %s com esse código.\n1) Adicionar estoque\n2) Passar um novo código\n",
 							produtos.findCod(codigodoCarro).getNome());
-					int esc = leInt("Sua escolha: ");
+					int esc = Uteis.leInt("Sua escolha: ");
 					if (esc == 1)
 						{mudaEstoque("adicionar");
 						break;}
@@ -78,10 +76,9 @@ public class Comercio {
 	}
 	public void mudaEstoque(String n) {
 		if(produtos.getProdutos().size() >0) {
-			int codAcesso = leInt("Digite o código de acesso do carro: ");
+			int codAcesso = Uteis.leInt("Digite o código de acesso do carro: ");
 			if (produtos.findCod(codAcesso) != null){
-				System.out.printf("Digite quantas unidades você deseja %s: ", n.toLowerCase());
-				int unidadesAModificar = Integer.parseInt(t.nextLine());
+				int unidadesAModificar = Uteis.leInt("Digite quantas unidades você deseja "+ n.toLowerCase()+": ");
 				if (n.toLowerCase().equals("adicionar")) produtos.adicionar(codAcesso, unidadesAModificar);
 				else if (n.toLowerCase().equals("vender")) produtos.vender(codAcesso, unidadesAModificar);
 			}else {
@@ -94,12 +91,11 @@ public class Comercio {
 	public void remover() {
 		if(produtos.getProdutos().size() >0) {
 			while (true) {
-				int codigodoCarro = leInt("Digite o código do carro: ");
+				int codigodoCarro = Uteis.leInt("Digite o código do carro: ");
 				if (produtos.findCod(codigodoCarro) != null) {
 					if(produtos.findCod(codigodoCarro).getEstoque() > 0){
-						System.out.printf("%s tem %d unidades em estoque. \nDeseja remover mesmo assim? ", 
-								produtos.findCod(codigodoCarro).getNome(),produtos.findCod(codigodoCarro).getEstoque());
-						String confirmacao = t.nextLine();
+						String confirmacao = Uteis.leString(produtos.findCod(codigodoCarro).getNome()+" tem "+
+						produtos.findCod(codigodoCarro).getEstoque()+ " unidades em estoque.\nDeseja remover mesmo assim? ");
 						if ((confirmacao.toLowerCase()).contains("s")) {
 							System.out.printf("%s e todo o seu estoque foi removido.\n", produtos.findCod(codigodoCarro).getNome());
 		
